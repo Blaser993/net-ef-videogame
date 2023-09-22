c326103376c40006975b94620ba17e2cb21644ac
Blaser993 <corradodepinto.design@gmail.com>
2023-09-22 15:21:37 +0200

    entità create

---
+++ net-ef-videogame/Sfotware_house.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    public class Sfotware_house
+    {
+        //[Key]
+        public long S_house_Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country{ get; set; }
+
+        public List<Videogame> Videogames { get; set; }
+        public Videogame videogame { get; set; }
+    }
+}
+}
---
+++ net-ef-videogame/Videogame.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    //[Table("videogame")]
+    //[Index(nameof(id) IsUnique = true]
+    public class Videogame
+    {
+        //[Key]
+        public long VideogameId { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Overview { get; set; }
+
+        public DateTime Relase_date { get; set; }
+
+
+        public long S_house_Id { get; set; }
+    
+    }
+}
